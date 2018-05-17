63bf85b8c32db809b1166199dd6546a56143b8d1
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-05-17 11:46:07 +0800

    First commit

--- Bitcoin-Ticker-App/ViewController.swift
+++ Bitcoin-Ticker-App/ViewController.swift
@@ -10,16 +10,76 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    var finalURL = ""
+    
+    //Pre-setup IBOutlets
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
+    //TODO: Place your 3 UIPickerView delegate methods here
+    
+    
+    
+    
+    
+    
+    
+    //
+    //    //MARK: - Networking
+    //    /***************************************************************/
+    //
+        func getWeatherData(url: String, parameters: [String : String]) {
+    
+            Alamofire.request(url, method: .get, parameters: parameters)
+                .responseJSON { response in
+                    if response.result.isSuccess {
+    
+                        print("Sucess! Got the weather data")
+                        let weatherJSON : JSON = JSON(response.result.value!)
+    
+                        self.updateWeatherData(json: weatherJSON)
+    
+                    } else {
+                        print("Error: \(String(describing: response.result.error))")
+                        self.bitcoinPriceLabel.text = "Connection Issues"
+                    }
+                }
+    
+        }
+    //
+    //
+    //
+    //
+    //
+    //    //MARK: - JSON Parsing
+    //    /***************************************************************/
+    //
+    //    func updateWeatherData(json : JSON) {
+    //
+    //        if let tempResult = json["main"]["temp"].double {
+    //
+    //        weatherData.temperature = Int(round(tempResult!) - 273.15)
+    //        weatherData.city = json["name"].stringValue
+    //        weatherData.condition = json["weather"][0]["id"].intValue
+    //        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
+    //        }
+    //
+    //        updateUIWithWeatherData()
+    //    }
+    //
+    
+    
+    
 
 }
 
